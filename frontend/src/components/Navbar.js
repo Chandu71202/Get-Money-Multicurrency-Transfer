d7d24a7c601259e74035a387064b93ceece79f9c
ESWAR CHAND <chandu71202@gmail.com>
2023-09-26 16:54:39 +0530

    feat: added the basic structure of Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../styles/Navbar.css';
 
 export default function Navbar() {
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="navbar-left">
+        <div className={`hamburger-menu ${menuActive ? 'active' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className="logo">GET-Money</div>
+      </div>
+      <div className={`navbar-right ${menuActive ? 'active' : ''}`}>
+        <ul className="navbar-menu">
+          <li><a href="#">About</a></li>
+        </ul>
+        <div className="buttons">
+          <button className="login-button">Login</button>
+          <button className="get-started-button">Get Started</button>
+        </div>
+      </div>
+    </nav>
+  );
 }
