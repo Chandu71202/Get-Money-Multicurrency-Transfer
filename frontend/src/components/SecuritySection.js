22fcc659bd20bc6e20440af06e2b3fdf84f138b0
Mohammad Aman Chaudhry <chaudhrya12@gmail.com>
2023-09-28 18:43:53 +0530

    feat:(Aman) Added more stuff to home

--- frontend/src/components/SecuritySection.js
+++ frontend/src/components/SecuritySection.js
@@ -4,18 +4,25 @@ import '../styles/SecuritySection.css'
 export default function SecuritySection() {
   return (
     <div>
-        <section className="securitySection">
-        <div className="security">
-          <h2>MultiCurrency Accounts</h2>
-          <p>Manage multiple currencies in one account</p>
-        </div>
-        <div className="feature">
-          <h2>Secure Transactions</h2>
-          <p>Your transactions are encrypted and secure</p>
-        </div>
-        <div className="feature">
-          <h2>24/7 Support</h2>
-          <p>Our support team is always ready to assist you</p>
+      <section className="securitySection">
+        <div className='security'>
+          <div >
+            <h1 className='securityHeader'>Always ahead of thieves</h1>
+            <div>
+              <p className='security-p'>Every month, our customers trust us to move around $9 billion of their money. Here are some of the important ways we protect them.</p>
+            </div>
+            <div className='row'>
+              <ul className='security-ul'>
+                <li>We use a strong, proven and secure encryption algorithm</li>
+                <li>Our dedicated fraud and security teams work to keep your money safe</li>
+                <li>We use 2-factor authentication to protect your account</li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <img alt='lock' src='https://wise.com/web-art/assets/illustrations/lock-large@1x.webp' />
+          </div>
         </div>
       </section>
     </div>
