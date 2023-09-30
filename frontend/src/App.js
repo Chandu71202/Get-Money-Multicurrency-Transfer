7ca683bc0605cb0298f99705d2d52de4e5642c71
1192816 <rajeshwari1612001@gmail.com>
2023-09-30 10:45:48 +0530

    feat: Raji Added dashboard page outline

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Home from './components/Home';
 import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 import Login from './components/Login'; // Import the Login component
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TransferMoney from './components/TransferMoney';
+
+
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} /> 
+          <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/transferMoney' element={<TransferMoney />} />'
         </Routes>
       </Router>
