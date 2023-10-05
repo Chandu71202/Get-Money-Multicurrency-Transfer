e63277f0f334aee104b875c99c43b1a32b48a75a
parimala8 <77389746+parimala8@users.noreply.github.com>
2023-10-05 13:06:39 +0530

    Parimala made a few changes to the faqs section of the homepage and changed some stylings of login page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,6 +5,7 @@ import CarouselSection from './CarouselSection'
 import Services from './Services'
 import SecuritySection from './SecuritySection'
 import CountriesSection from './CountriesSection'
+import MyFAQ from './MyFAQ'
 import Footer from './Footer'
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
       <SecuritySection />
       <CurrentExchangeRates />
       <CountriesSection />
+      <MyFAQ />
       <Footer />
     </div>
 
