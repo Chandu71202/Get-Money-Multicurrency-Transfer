51ee669d0e2000c8ccd710f3fcc3656a47a366db
ESWAR CHAND <chandu71202@gmail.com>
2023-10-09 19:25:50 +0530

    Revert "Test cases for backend"
    This reverts commit da2d50f8143fbba3b1ea1113ad82c740492c334c.

---
+++ backend/User/src/test/java/com/example/GETMoney/GetMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GETMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
